0d4a6be7e7326255c2d91a027da8b9e128f50cbb
Awez <awezbaigmirza@gmail.com>
2023-12-07 15:01:29 +0530

    Major Update

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const apiKey = "X1QIYHSGZWGNAQB8PPGIWSFEQ67H5EV9S9";
-const usdtContractAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
-const usdcContractAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
-const maticContractAddress = "0x0000000000000000000000000000000000001010";
+const polyApiKey = "X1QIYHSGZWGNAQB8PPGIWSFEQ67H5EV9S9";
+const etherApiKey = "N6AAJCSUEIX8X1FVJMH9DR34HJQ6QGAU5P";
+const polyUsdtContractAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+const polyUsdcContractAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+const polyMaticContractAddress = "0x0000000000000000000000000000000000001010";
+
+const etherUsdtContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const etherUsdcContractAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const etherMaticContractAddress = "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0";
+
+let coinAddress = "";
 
 const getCoin = (coin) => {
-    if (coin == "USDT") return usdtContractAddress;
-    else if (coin == "USDC") return usdcContractAddress;
-    else return maticContractAddress;
+    if (coin == "USDT") return polyUsdtContractAddress;
+    else if (coin == "USDC") return polyUsdcContractAddress;
+    else return polyMaticContractAddress;
 }
+
+const setEthCoin = (coin) => {
+    if (coin == "USDT") return etherUsdtContractAddress;
+    else if (coin == "USDC") return etherUsdcContractAddress;
+    else return etherMaticContractAddress;
+}
+
+// let url = `https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${coinAddress}&address=${address}&tag=latest&apikey=${polyApiKey}`
+// const setChain = () => {
+//     url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${coinAddress}&address=${address}&tag=latest&apikey=${etherApiKey}`
+// }
 
 let amounts = [];
 async function inptStringToArray(fullString) {
@@ -26,14 +44,18 @@ async function inptStringToArray(fullString) {
     if (!(substr == "" || substr == " ")) amounts.push(substr);
 }
 
-
-async function getBalance(address, coinAddress) {
+async function getBalance(address, coinAddress, chain) {
     try {
-        const url = `https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${coinAddress}&address=${address}&tag=latest&apikey=${apiKey}`
+        if (chain == "Ethereum") {
+            url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${coinAddress}&address=${address}&tag=latest&apikey=${etherApiKey}`;
+        }
+        else {
+            url = `https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${coinAddress}&address=${address}&tag=latest&apikey=${polyApiKey}`;
+        }
         const gotVal = await axios.get(url);
         console.log(gotVal.data);
         if (gotVal.data.status == 1) {
-            if (coinAddress != maticContractAddress) {
+            if (coinAddress != polyMaticContractAddress || coinAddress != etherMaticContractAddress) {
                 return (gotVal.data.result * 0.000001);
             }
             else {
@@ -52,17 +74,25 @@ let tableCreated = 0;
 const btn = document.getElementById("btn");
 btn.addEventListener("click", async () => {
     const addresses = document.querySelector("#inpt");
+    const chain = document.querySelector("#chain").value;
+
     await inptStringToArray(addresses.value);
+
     addresses.value = "";
     console.log(amounts);
 
     const coin = document.querySelector("#coin").value;
     console.log(coin);
-    const coinAddress = getCoin(coin);
+    if (chain == "Ethereum") {
+        coinAddress = setEthCoin(coin)
+    }
+    else {
+        coinAddress = getCoin(coin)
+    }
 
     for (let address of amounts) {
 
-        const amount = await getBalance(address, coinAddress);
+        const amount = await getBalance(address, coinAddress, chain);
         const body = document.querySelector("body");
         if (amount == -1) {
             const h2 = document.createElement("h2");
@@ -73,13 +103,13 @@ btn.addEventListener("click", async () => {
             if (!tableCreated) {
                 const table = document.createElement("table");
                 const tr = document.createElement("tr");
-                tr.innerHTML = `<th> Wallet Address </th> <th> Amount </th> <th> Coin</th>`;
+                tr.innerHTML = `<th> Wallet Address </th> <th> Amount </th> <th> Coin</th> <th> Blockchain </th>`;
                 table.appendChild(tr);
                 body.append(table);
             }
             const table = document.querySelector("table");
             const tr = document.createElement("tr");
-            tr.innerHTML = ` <td> ${address} </td> <td> <b> ${amount} </td> </b>  <td> ${coin}</td>`;
+            tr.innerHTML = ` <td> ${address} </td> <td> <b> ${amount} </td> </b>  <td> ${coin}</td> <td> ${chain}`;
             table.appendChild(tr);
             tableCreated = 1;
         }
